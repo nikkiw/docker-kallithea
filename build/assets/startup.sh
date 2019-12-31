478d9d0d78a7d0fe4d752e53beaa53086fb9c19b
toras9000 <toras9000@example.com>
2019-12-31 19:18:12 +0900

    update Dockerfile

--- build/assets/startup.sh
+++ build/assets/startup.sh
@@ -3,6 +3,7 @@
 # generate locale
 LANG=${KALLITHEA_LOCALE:-"en_US.UTF-8"}
 locale-gen --lang ${LANG}
+update-locale LANG=${LANG}
 export LANG
 
 # settings file path
@@ -18,17 +19,17 @@ if [ ! -e $KALLITHEA_INI ]; then
     if [ -n "$KALLITHEA_EXTERNAL_DB" ]; then
         echo "Setting db connection string..."
         DB_ESC=$(echo "$KALLITHEA_EXTERNAL_DB" | sed -e 's/[\/&]/\\&/g')
-        sed -i "s/^sqlalchemy\.url\s*=.*$/sqlalchemy.url = ${DB_ESC}/1" $KALLITHEA_INI
+        sed -ri "s/^\\s*sqlalchemy\\.url\\s*=.*\$/sqlalchemy.url = ${DB_ESC}/1" $KALLITHEA_INI
     fi
 
     # ui language
     if [ -n "$KALLITHEA_LANG" ]; then
         echo "Setting language to ${KALLITHEA_LANG}"
-        sed -i "s/^i18n\.lang\s*=.*$/i18n.lang = ${KALLITHEA_LANG}/1" $KALLITHEA_INI
+        sed -ri "s/^\\s*i18n\\.lang\\s*=.*\$/i18n.lang = ${KALLITHEA_LANG}/1" $KALLITHEA_INI
     fi
 
     # cookie expiration time
-    sed -ri 's/^(beaker.session.timeout =.*)$/\1\nbeaker.session.cookie_expires = 2592000/1' $KALLITHEA_INI
+    sed -ri "s/^\\s*(beaker\\.session\\.timeout\\s*=.*)\$/\\1\nbeaker.session.cookie_expires = 2592000/1" $KALLITHEA_INI
 
     # initialize database
     echo "Creating database..."
@@ -45,11 +46,11 @@ if [ -n "$KALLITHEA_REPOSORT_IDX" ]; then
     KRS_IDX=$KALLITHEA_REPOSORT_IDX
     KRS_ODR=${KALLITHEA_REPOSORT_ORDER:-"asc"}
     PATCH_FILE=/usr/local/lib/python2.7/dist-packages/kallithea/templates/index_base.html
-    sed -i "s/^                order: \[\[1, \"asc\"\]\],$/                order: [[${KRS_IDX}, \"${KRS_ODR}\"]],/1" $PATCH_FILE
+    sed -ri "s/^                order: \\[\\[1, \"asc\"\\]\\],\$/                order: [[${KRS_IDX}, \"${KRS_ODR}\"]],/1" $PATCH_FILE
 fi
 
-echo "Start kallithea..."
-gearbox serve -c $KALLITHEA_INI &
+echo "Start nginx reverse proxy ..."
+nginx -g "daemon off;" &
 
-echo "Start nginx reverse proxy..."
-nginx -g "daemon off;"
+echo "Start kallithea ..."
+gearbox serve -c $KALLITHEA_INI
